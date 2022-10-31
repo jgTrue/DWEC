b4c6c2c007d29940c7cc6b513251ea3e38d638a0
Jairo GB <jgtrue@protonmail.com>
2022-10-31 13:54:26 +0100

    EJ 11 12 13 upload

---
+++ Fundamentos/011ifStringCero/011ifStringCero.js
@@ -0,0 +1,5 @@
+/*011ifStringCero.js/.html: ¿Se mostrará el alert?
+if ("0") {
+  alert( '¿me ejecuto o no?' );
+}*/
+//? Si se ejecuta ya que es un string definido por tanto es evaludo como true, debería esta vacío para que sucediera lo contrario
---
+++ Fundamentos/012nombreJS/012nombreJS.js
@@ -0,0 +1,14 @@
+/*012nombreJS.js/.html: Usando el constructor if..else, escribe el código que pregunta: 
+‘¿Cuál es el nombre “oficial” de JavaScript?’ Si el visitante escribe “ECMAScript”, 
+entonces muestra: “¡Correcto!”, de lo contrario muestra: “¿No lo sabes? 
+¡ECMAScript!”*/
+
+"use stric";
+
+let nombreOficial = "ECMAScript"
+
+if (nombreOficial == prompt("¿Cúal es el nombre “oficial” de JavaScript?")){
+    alert("¡Correcto!")
+}else{
+    alert("¿No lo sabes?")
+}
