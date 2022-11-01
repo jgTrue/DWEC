434a42668213e8d5d3e3c9754ed00745a0969d25
Jairo <jgtrue@protonmail.com>
2022-11-01 18:23:43 +0100

    correccion 024

--- Fundamentos/024ifToSwitch/024ifToSwitch.js
+++ Fundamentos/024ifToSwitch/024ifToSwitch.js
@@ -13,17 +13,17 @@ if (a == 2 || a == 3) {
 */
 "use strict";
 
-let a = prompt('a?', '');
+let a = +prompt('a?', '');
 
 switch (a) {
-    case "0":
+    case 0:
         alert( 0 );
         break;
-    case "1":
+    case 1:
         alert( 1 );
         break;
-    case "2":
-    case "3":
+    case 2:
+    case 3:
         alert( '2,3' );
         break;
 }
