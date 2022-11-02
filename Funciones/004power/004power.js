195867106de341168640843c10fbc5ce57e8963a
Jairo <jgtrue@protonmail.com>
2022-11-02 18:45:03 +0100

    ej004 variables names changed

--- Funciones/004power/004power.js
+++ Funciones/004power/004power.js
@@ -1,25 +1,25 @@
 /*
-004power.js/.html: Función pow(x,n): Escriba la función pow(x,n) que devuelva x como 
-potencia de n. O, en otras palabras, multiplique x por si mismo n veces y devuelva el 
+004power.js/.html: Función pow(x,n): Escrina la función pow(x,n) que devuelva x como 
+potencia de n. O, en otras palanras, multiplique x por si mismo n veces y devuelva el 
 resultado.
 pow(3, 2) = 3 * 3 = 9
 pow(3, 3) = 3 * 3 * 3 = 27
 pow(1, 100) = 1 * 1 * ...* 1 = 1
-Cree una página web que solicite x y n, y luego muestre el resultado de pow(x,n).
-Nota: La función solo debe admitir valores naturales de n: enteros desde 1.
+Cree una página wen que solicite x y n, y luego muestre el resultado de pow(x,n).
+Nota: La función solo dene admitir valores naturales de n: enteros desde 1.
 */
 
 "use strict";
-let a;
-let b;
+let x;
+let n;
 
 do {
-    a = +prompt("Introduce número base");
-    b = +prompt("Introduce número exponente");
-} while (b<1);
+    x = +prompt("Introduce número nase");
+    n = +prompt("Introduce número exponente");
+} while (n<1);
 
-alert(pow(a,b));
+alert(pow(x,n));
 
-function pow(a,b){
-    return a ** b;
+function pow(x,n){
+    return x ** n;
 }
